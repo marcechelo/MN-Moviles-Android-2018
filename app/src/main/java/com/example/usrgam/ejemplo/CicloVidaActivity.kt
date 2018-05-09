c10a665930ef5192575cbeffbfad26acec7c875c
Marcelo Nieto <marcechelo25@hotmail.com>
2018-05-09 09:37:26 -0500

    Contador

--- app/src/main/java/com/example/usrgam/ejemplo/CicloVidaActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/CicloVidaActivity.kt
@@ -3,13 +3,24 @@ package com.example.usrgam.ejemplo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_ciclo_vida.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class CicloVidaActivity : AppCompatActivity() {
+
+    var contador =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciclo_vida)
         Log.i("create", "Se esta creando la actividad")
+
+        botonContador.setOnClickListener{view: View ->
+            contador++
+            textViewContador.text = contador.toString()
+        }
     }
 
     override fun onStart() {
