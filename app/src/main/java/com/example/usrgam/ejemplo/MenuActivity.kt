6b64117aac6d2b95fe27a6dcbb4827c1c9a7ee3e
Marcelo Nieto <marcechelo25@hotmail.com>
2018-05-23 10:14:59 -0500

    popup menu

--- app/src/main/java/com/example/usrgam/ejemplo/MenuActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/MenuActivity.kt
@@ -3,10 +3,14 @@ package com.example.usrgam.ejemplo
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
 
 import kotlinx.android.synthetic.main.activity_menu.*
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +18,59 @@ class MenuActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val popup = PopupMenu(this,view)
+            popup.setOnMenuItemClickListener(this)
+            val inflar = popup.menuInflater
+            inflar.inflate(R.menu.pop_up_menu,popup.menu)
+            popup.show()
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.item_menu_pokemon -> {
+                Log.i("menu", "Menu pokemon")
+                return true
+            }
+            R.id.item_menu_settings -> {
+                Log.i("menu", "Menu settings")
+                return true
+            }
+            R.id.item_menu_nuevo1 -> {
+                Log.i("menu", "Menu nuevo")
+                return true
+            }
+            R.id.item_menu_aceptar-> {
+                Log.i("menu", "Menu aceptar")
+                return true
+            }
+            R.id.item_menu_cancelar -> {
+                Log.i("menu", "Menu cancelar")
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onMenuItemClick(item:MenuItem):Boolean{
+        when (item.getItemId()) {
+            R.id.item_menu_aceptar-> {
+                Log.i("menu", "Menu aceptar")
+                return true
+            }
+            R.id.item_menu_cancelar -> {
+                Log.i("menu", "Menu cancelar")
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 
