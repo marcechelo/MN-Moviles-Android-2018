aee459d48a9b744dac5cf65c73043f0a2ca10491
Marcelo Nieto <marcechelo25@hotmail.com>
2018-05-23 10:59:28 -0500

    dialogo

--- app/src/main/java/com/example/usrgam/ejemplo/DialogoActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/DialogoActivity.kt
@@ -1,8 +1,11 @@
 package com.example.usrgam.ejemplo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_dialogo.*
 
@@ -14,6 +17,39 @@ class DialogoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
+            val builder = AlertDialog.Builder(this)
+            val seleccionUsuario = booleanArrayOf(true,false,false,false)
+
+            //builder.setMessage("¿Está seguro?")
+            val listaItems = resources.getStringArray(R.array.string_array_opciones_dialogo)
+            builder.setTitle("Escoja un numero")
+//            builder.setSingleChoiceItems(listaItems,-1,DialogInterface.OnClickListener { dialog, which ->
+//                Log.i("dialogo", "Acepto")
+//            })
+
+            builder.setMultiChoiceItems(listaItems,seleccionUsuario,DialogInterface.OnMultiChoiceClickListener {
+                dialog: DialogInterface,
+                i: Int,
+                b -> Log.i("dialogo","escogio algo $i")  }
+            )
+            //builder.setSingleChoiceItems()
+            builder.setPositiveButton(
+                    "Aceptar",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Log.i("dialogo", "Acepto")
+                    }
+            )
+            builder.setNegativeButton(
+                    "Cancelar",
+                    null
+            )
+            val dialogo = builder.create()
+            dialogo.show()
+
+
+
+
+
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
--- app/src/main/java/com/example/usrgam/ejemplo/MenuActivity.kt
+++ app/src/main/java/com/example/usrgam/ejemplo/MenuActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.PopupMenu
 
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.content_menu.*
+import java.nio.file.Files.delete
+import android.R.id.edit
+import android.widget.AdapterView.AdapterContextMenuInfo
+
+
 
 class MenuActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 
@@ -17,12 +26,33 @@ class MenuActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
         setContentView(R.layout.activity_menu)
         setSupportActionBar(toolbar)
 
+        val numeros = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numeros)
+
+        list_view_numeros.adapter = adapter
+
+        registerForContextMenu(list_view_numeros)
+
+
+
         fab.setOnClickListener { view ->
-            val popup = PopupMenu(this,view)
+
+            val popup = PopupMenu(this, view)
+
             popup.setOnMenuItemClickListener(this)
-            val inflar = popup.menuInflater
-            inflar.inflate(R.menu.pop_up_menu,popup.menu)
+
+            val inflater = popup.menuInflater
+
+            inflater.inflate(R.menu.pop_up_menu, popup.menu)
+
             popup.show()
+
+
+
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+
 
         }
     }
@@ -34,44 +64,84 @@ class MenuActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
         when (item.getItemId()) {
             R.id.item_menu_pokemon -> {
                 Log.i("menu", "Menu pokemon")
                 return true
             }
             R.id.item_menu_settings -> {
-                Log.i("menu", "Menu settings")
+                Log.i("menu", "Menu Settings")
                 return true
             }
             R.id.item_menu_nuevo1 -> {
-                Log.i("menu", "Menu nuevo")
+                Log.i("menu", "Menu Nuevo 1")
                 return true
             }
-            R.id.item_menu_aceptar-> {
-                Log.i("menu", "Menu aceptar")
+            R.id.item_menu_aceptar -> {
+                Log.i("menu", "Aceptar")
                 return true
             }
             R.id.item_menu_cancelar -> {
-                Log.i("menu", "Menu cancelar")
+                Log.i("menu", "Cancelar")
                 return true
             }
-            else -> return super.onOptionsItemSelected(item)
+            else -> {
+                Log.i("menu", "Todos los demas")
+                return super.onOptionsItemSelected(item)
+            }
         }
     }
 
-    override fun onMenuItemClick(item:MenuItem):Boolean{
+    override fun onMenuItemClick(item: MenuItem): Boolean {
         when (item.getItemId()) {
-            R.id.item_menu_aceptar-> {
-                Log.i("menu", "Menu aceptar")
+            R.id.item_menu_aceptar -> {
+                Log.i("menu", "Aceptar")
                 return true
             }
             R.id.item_menu_cancelar -> {
-                Log.i("menu", "Menu cancelar")
+                Log.i("menu", "Cancelar")
                 return true
             }
-            else -> return super.onOptionsItemSelected(item)
+            else -> {
+                Log.i("menu", "Todos los demas")
+                return super.onOptionsItemSelected(item)
+            }
         }
     }
+
+    override fun onCreateContextMenu(
+            menu: ContextMenu,
+            v: View,
+            menuInfo: ContextMenu.ContextMenuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.context_menu, menu)
+
+
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterContextMenuInfo
+        when (item.getItemId()) {
+            R.id.item_menu_guardar -> {
+                Log.i("menu", "Guardar")
+                return true
+            }
+            R.id.item_menu_editar -> {
+                Log.i("menu", "Editar")
+                return true
+            }
+            R.id.item_menu_borrar -> {
+                Log.i("menu", "Borrar")
+                return true
+            }
+            else -> {
+                Log.i("menu", "Todos los demas")
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
 
 }
